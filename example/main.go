77a195be5ea96cece4aeff4aaf252ca3549c31c4
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2021-07-04 09:39:48 +0900

    fix: example/main.go

--- example/main.go
+++ example/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -8,10 +9,11 @@ import (
 )
 
 type User struct {
-	Name     string
-	Age      int
-	Address  *string
-	Birthday time.Time
+	Name         string
+	Age          int
+	Address      *string
+	Birthday     time.Time
+	RegisterDate time.Time
 }
 
 type UserUpdate struct {
@@ -19,7 +21,24 @@ type UserUpdate struct {
 	Age              *int
 	Address          *string
 	Birthday         *time.Time
+	RegisterDate     string
 	NotExistingField string
+}
+
+func prettyJson(v interface{}) string {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return err.Error()
+	}
+	return string(b)
+}
+
+func mapKeys(v map[string]partial.Field) []string {
+	keys := make([]string, 0, len(v))
+	for k := range v {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
 func main() {
@@ -35,10 +54,11 @@ func main() {
 	now := time.Now()
 
 	userUpdate := UserUpdate{
-		Name:     "Updated Name",
-		Age:      &age,
-		Address:  &address,
-		Birthday: &now,
+		Name:         "Updated Name",
+		Age:          &age,
+		Address:      &address,
+		Birthday:     &now,
+		RegisterDate: "2016-01-01",
 	}
 
 	updater, err := partial.NewUpdater(&user)
@@ -53,14 +73,21 @@ func main() {
 		fmt.Printf("Error: %v\n", err) // Error: update fields failure
 	}
 
-	fmt.Printf("Updated user: %+v\n", user)
-	// => Updated user: {Name:Updated Name Age:31 Address:0xc000010230 Birthday:2021-07-04 09:16:12.458143 +0900 JST m=+0.000085842}
-	fmt.Printf("updated fields: %d\n", len(updater.UpdatedFields))
-	// => updated fields: 3
-	fmt.Printf("skipped fields: %d\n", len(updater.SkippedFields))
-	// => skipped fields: 1
-	fmt.Printf("not found fields: %d\n", len(updater.NotFoundFields))
-	// => not found fields: 1
-	fmt.Printf("not assignable fields: %d\n", len(updater.NotAssignableFields))
-	// => not assignable fields: 0
+	fmt.Printf("Updated user:\n%s\n", prettyJson(user))
+	// Updated user:
+	// {
+	//   "Name": "Updated Name",
+	//   "Age": 31,
+	//   "Address": "123 Main Street",
+	//   "Birthday": "2021-07-04T09:33:00.410106+09:00",
+	//   "RegisterDate": "0001-01-01T00:00:00Z"
+	// }
+	fmt.Printf("updated fields: %+v\n", mapKeys(updater.UpdatedFields))
+	// updated fields: [Birthday Name Age Address]
+	fmt.Printf("skipped fields: %+v\n", mapKeys(updater.SkippedFields))
+	// skipped fields: []
+	fmt.Printf("not found fields: %+v\n", mapKeys(updater.NotFoundFields))
+	// not found fields: [NotExistingField]
+	fmt.Printf("not assignable fields: %+v\n", mapKeys(updater.NotAssignableFields))
+	// not assignable fields: [RegisterDate]
 }
